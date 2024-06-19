8694f50ec4ca77f8f3b9c2c7b609dceca4161875
Iran Pascual León Hernández <l21200903@pachuca.tecnm.mx>
2024-06-19 02:10:39 -0600

    TA BIEN EN 3D

---
+++ Js/CondicionesIniciales.js
@@ -0,0 +1,16 @@
+var avance = 0;
+var girando = 0;
+var angulo = 0;
+var container, stats, controls;
+var camera, scene, renderer, light;
+var personaje = new THREE.Object3D();
+var posx = 0;
+var posy = 0;
+var posz = 0;
+var mixer;
+var numeroCosas = 40;
+var cubos = [];
+var esferas = [];
+var cilindros = [];
+var clock = new THREE.Clock();
+var deltaTime = clock.getDelta();
